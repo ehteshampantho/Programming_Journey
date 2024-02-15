16557c5a7d71150b154ddd322f14e429403f4aa7
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-02-15 18:40:52 +0600

    dijkstra algo

---
+++ Algorithm/Module 06/dijkstra_optimized.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 100;
+vector<pair<int,int>> v[N];
+int dis[N];
+
+class cmp{
+    public:
+        bool operator()(pair<int,int> a,pair<int,int> b){
+            return a.second > b.second;
+        }
+};
+
+void dijkstra(int src){
+    priority_queue<pair<int,int>,vector<pair<int,int>>,cmp> pq;
+    pq.push({src,0});
+    dis[src] = 0;
+    
+    while(!pq.empty()){
+        pair<int,int> parent = pq.top();
+        pq.pop();
+        
+        int node = parent.first;
+        int cost = parent.second;
+        
+        for(pair<int,int> child: v[node]){
+            int childNode = child.first;
+            int childCost = child.second;
+            if(cost + childCost < dis[childNode]){
+                dis[childNode] = cost + childCost;
+                pq.push({childNode,dis[childNode]});
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    
+    while(e--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        v[a].push_back({b,c});
+        v[b].push_back({a,c});
+    }
+    for(int i=0;i<n;i++){
+        dis[i] = INT_MAX;
+    }
+    
+    dijkstra(0);
+    
+    // priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> sq;
+    // sq.push({1,10});
+    // sq.push({2,5});
+    // sq.push({4,7});
+    // sq.push({3,6});
+    
+    // while(!sq.empty()){
+    //     cout<< sq.top().first << " " << sq.top().second << endl;
+    //     sq.pop();
+    // }
+    
+    for(int i=0;i<n;i++){
+        cout << i << "->" << dis[i] << endl;
+    }
+    
+    return 0;
+}
