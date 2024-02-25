21f11d5751cd60c8b86988036fc9ab9cf98d6678
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-02-26 00:44:23 +0600

    dsu union by rank

---
+++ Algorithm/Module 10/dsu_union_by_rank.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5+5;
+int par[N];
+int group[N];
+int level[N];
+
+void dsu_initialize(int n){
+    for(int i=0;i<n;i++){
+        par[i] = -1;
+        level[i] = 0;
+    }
+}
+
+int dsu_find(int node){
+    if(par[node]==-1) return node;
+    int leader = dsu_find(par[node]);
+    par[node] = leader;
+    return leader;
+}
+
+void dsu_union_by_level(int node1, int node2){
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    if(level[leaderA] > level[leaderB]){
+        par[leaderB] = leaderA;
+    }
+    else if(level[leaderB] > level[leaderA]){
+        par[leaderA] = leaderB;
+    }
+    else{
+        par[leaderB] = leaderA;
+        level[leaderA] += level[leaderB];
+    }
+}
+
+int main(){
+    dsu_initialize(7);
+    dsu_union_by_level(1,2);
+    dsu_union_by_level(2,3);
+    dsu_union_by_level(4,5);
+    dsu_union_by_level(5,6);
+    cout << dsu_find(2) << endl;
+    cout << dsu_find(4) << endl;
+    dsu_union_by_level(1,6);
+    cout<< dsu_find(3) << endl;
+    cout<< dsu_find(6) << endl;
+    return 0;
+}
