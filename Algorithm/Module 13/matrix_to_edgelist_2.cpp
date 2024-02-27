5d9d2c879ce879236a90e8b23b0386723ee7fc3b
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-02-28 00:03:54 +0600

    matrix to edgelist weighted and unweighted

---
+++ Algorithm/Module 13/matrix_to_edgelist_2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<pair<int,int>> edgeList;
+    int mat[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>mat[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(mat[i][j]==1 && i!=j){
+                edgeList.push_back({i,j});
+            }
+        }
+    }
+    for(pair<int,int> child: edgeList){
+        cout<<child.first<<" "<<child.second<<endl;
+    }
+    
+    return 0;
+}
