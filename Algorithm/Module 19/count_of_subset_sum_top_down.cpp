7d1774552b0fe32b74bfc184b306521c170613ad
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-03-06 13:23:16 +0600

    count of subset sum

---
+++ Algorithm/Module 19/count_of_subset_sum_top_down.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int dp[1005][1005];
+
+int count_subset_sum(int n,int a[],int s){
+
+    if(n==0){
+        if(s==0){
+            return 1;
+        }
+        else return 0;
+    }
+
+    if(dp[n][s] != -1){
+        return dp[n][s];
+    }
+
+    if(a[n-1] <= s){
+        int op1 = count_subset_sum(n-1,a,s-a[n-1]);
+        int op2 = count_subset_sum(n-1,a,s);
+        dp[n][s] = op1 + op2;
+        return dp[n][s]; 
+    }
+    else{
+        int op2 = count_subset_sum(n-1,a,s);
+        return dp[n][s] = op2;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    int s;
+    cin>>s;
+
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=s;j++){
+            dp[i][j] = -1;
+        }
+    }
+
+    cout<< count_subset_sum(n,a,s) << '\n';
+    return 0;
+}
