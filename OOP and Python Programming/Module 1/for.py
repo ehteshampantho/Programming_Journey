0473228d4722caab03f6ee91978586a1a9b06783
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-04-15 22:07:23 +0600

    for loop

---
+++ OOP and Python Programming/Module 1/for.py
@@ -0,0 +1,25 @@
+# nums = []
+# for(int i=0; i<5; i++){
+#     int num = nums[i]
+# }
+
+# numbers = [5,10,15,20,25]
+# sum = 0
+# for num in numbers:
+#     print(num)
+#     sum = sum + num
+#     if(sum > 20):
+#         print("bigger sum", sum)
+
+# print(sum)
+
+# text = "Pagla Hawar"
+# for char in text:
+#     print(char)
+
+# for i in range(1,10,2):
+#     print(i)
+
+friends = ["nobel","turan","rakib"]
+for idx,friend in enumerate(friends):
+    print(idx,friend)
