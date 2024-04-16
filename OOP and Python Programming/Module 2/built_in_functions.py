88b0b1e02dcd91a056a043e8e79381fa21bd2b6e
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-04-17 02:00:46 +0600

    built-in function

---
+++ OOP and Python Programming/Module 2/built_in_functions.py
@@ -0,0 +1,6 @@
+# highest = max(45,98,46,87,96,41)
+highest = max([45,98,46,87,96,41])
+smallest = min([45,98,46,87,96,41])
+total = sum([45,98,46,87,96,41])
+count = len([46,97])
+print(highest, smallest, total, count)
