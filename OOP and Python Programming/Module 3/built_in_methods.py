76757095bb0ebf64ff925a5823adfc065d20b92f
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-04-19 19:51:51 +0600

    built in methods

---
+++ OOP and Python Programming/Module 3/built_in_methods.py
@@ -0,0 +1,11 @@
+from math import *
+from random import *
+from time import sleep
+
+# result = ceil(4.6)
+result = floor(4.999)
+print(result)
+print(random())
+print(randint(1,99))
+sleep(4)
+print(choice(['sakib','mash','mush','rid','musta']))
