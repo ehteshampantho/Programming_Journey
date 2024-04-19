58e6cc801663cf584a60d4b096ebb7fb89c54be6
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-04-19 20:15:20 +0600

    built in

--- OOP and Python Programming/Module 3/built_in_methods.py
+++ OOP and Python Programming/Module 3/built_in_methods.py
@@ -1,6 +1,9 @@
 from math import *
 from random import *
 from time import sleep
+import time
+
+start_time = time.time()
 
 # result = ceil(4.6)
 result = floor(4.999)
@@ -9,3 +12,5 @@ print(random())
 print(randint(1,99))
 sleep(4)
 print(choice(['sakib','mash','mush','rid','musta']))
+
+print(time.time() - start_time)
