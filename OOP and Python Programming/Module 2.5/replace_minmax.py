383d65a37e2fe610b021f5fcf351b0a6415bcefc
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-04-19 14:07:26 +0600

    replace minmax

--- OOP and Python Programming/Module 2.5/replace_minmax.py
+++ OOP and Python Programming/Module 2.5/replace_minmax.py
@@ -1,24 +1,29 @@
 n = int(input())
-
+ 
 final = list(map(int,input().strip().split()))[:n]
 # print(final)
-
+ 
 mx = max(final)
+mx_idx = final.index(mx)
 mn = min(final)
-
-for i in final: #4,10,1,3,2,10
-    if i == mx:
-        print(mn,end=" ")
-    elif i == mn:
-        print(mx,end=" ")
-    else:
-        print(i,end=" ")
-
+mn_idx = final.index(mn)
+ 
+# for i in final: #4,10,1,3,2
+#     if i == mx:
+#         print(mn,end=" ")
+#     elif i == mn:
+#         print(mx,end=" ")
+#     else:
+#         print(i,end=" ")
+ 
 # final[final.index(mx)], final[final.index(mn)] = final[final.index(mn)] , final[final.index(mx)]
+ 
+temp = final[mx_idx]
+final[mx_idx] = final[mn_idx]
+final[mn_idx] = temp
 
-# temp = final[final.index(mx)]
-# final[final.index(mx)] = final[final.index(mn)]
-# final[final.index(mn)] = temp
+# print(final)
+ 
+print(*final)
 
-# print(*final)
 
