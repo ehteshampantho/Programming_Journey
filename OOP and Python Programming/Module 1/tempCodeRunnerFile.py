812089dd331aa29a438a242ac9ff621da11c8a74
ehteshampantho <ehteshamulkabir15@gmail.com>
2024-10-28 20:40:28 +0600

    variable

--- OOP and Python Programming/Module 1/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-print(5465)
-print('Python re dhore felsi, mamma!!!')
--- OOP and Python Programming/Module 1/variable.py
+++ OOP and Python Programming/Module 1/variable.py
@@ -1,8 +1,8 @@
 # variable_name = value
 age = 45
 interest_rate = 2.5
-name = "Jume e jo pathan"
-district = 'B Baria'
+name = "Jume e jo Pathan"
+district = "B Baria"
 is_single = True
 is_sleeping = False
 
@@ -14,9 +14,7 @@ We are exploring variables
 we know variables very well
 But we are using variables in Python
 right now. I am very very excitedly hungry.
-
 """
-
 
 print(age * interest_rate)
 print(district)
@@ -28,7 +26,6 @@ print(type(age))
 # print(type(district))
 # print(type(is_sleeping))
 
-
 print("Kodom Ali" + " " +"kacha badam")
-text = f"Kodom Ali {age} living in {district}.interesting with rate {interest_rate}%"
+text = f"Kodom Ali {age} living in {district}. interesting with rate {interest_rate}%"
 print(text) 
